9ea1e14ffd1fb5d3cd6784835b9ea2637ba56be5
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2021-11-08 23:31:55 +0530

    -Values and Variables in Scala

--- src/main/scala/lectures/ValuesVariablesTypes.scala
+++ src/main/scala/lectures/ValuesVariablesTypes.scala
@@ -13,4 +13,20 @@ object ValuesVariablesTypes extends App {
   val str: String = "hello";
   //Semicolons are allowed in scala
 
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  //Compiler complains if number too long
+  val aShort: Short = 1242
+  val aLong: Long = 1224345436456456L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  //variables
+  var aVariable: Int = 4
+  aVariable = 6 //Side Effects
+
+  //Key Takeaways
+  //1: Prefer vals over vars
+  //2: All vals and vars have types
+  //3: Compiler automatically infers types when omitted
 }
