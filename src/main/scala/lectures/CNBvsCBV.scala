156389146f21ddd84a9b2bb4258f1a1eb1130f1e
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2021-11-15 09:26:23 +0530

    -Call By Value v/s Call By Name in scala.

---
+++ src/main/scala/lectures/CNBvsCBV.scala
@@ -0,0 +1,27 @@
+package lectures
+
+object CNBvsCBV extends App {
+  /*
+  1.Value is computed before the call.
+  2.Same value is used everywhere.
+   */
+  def calledByValue(x: Long): Unit = {
+    println("by Value :" + x)
+    println("by Value:" + x)
+  }
+
+  /*
+  1.Expression is passed iterally.
+  2.Expression is evaluated at every use within.
+   */
+  def calledByName(x: => Long): Unit = {
+    println("by Name :" + x)
+    println("by Name :" + x)
+  }
+  calledByValue(System.nanoTime())
+  calledByName(System.nanoTime())
+  def infinite(): Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int) = println(x)
+  //printFirst(infinite(), 34)
+  printFirst(34, infinite())
+}
