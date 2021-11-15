f2ec685398b8e54cf4d1af511a8bfa41c95e7240
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2021-11-15 10:02:31 +0530

    -String operations in scala.

---
+++ src/main/scala/lectures/StringOps.scala
@@ -0,0 +1,33 @@
+package lectures
+
+object StringOps extends App {
+  val str: String = "Hello I am learning Scala"
+  println(str.charAt(2))
+  println(str.substring(7, 11))
+  println(str.split(" ").toList)
+  println(str.startsWith("Hello"))
+  println(str.replace(" ", "-"))
+  println(str.toLowerCase())
+  println(str.length)
+
+  val aNumberString = "45"
+  val aNumber = aNumberString.toInt
+  /*Scala Specific: String interpolators*/
+  println('a' +: aNumberString :+ 'z')
+  println(str.reverse)
+  println(str.take(2))
+
+  /*S-interpolators*/
+  val name = "David"
+  val age = 12
+  val greeting = s"Hello, my name is $name and  I am age $age years old"
+  val anothergreeting =
+    s"Hello, my name is $name and  I am age ${age + 1} years old"
+  println(anothergreeting)
+
+  /*F-interpolators*/
+  val speed = 1.2f
+  val myth = f"$name can eat $speed%2.2f burgers per minute"
+  println(myth)
+
+}
