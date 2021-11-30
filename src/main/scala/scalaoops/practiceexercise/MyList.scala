2606e2f9816bda578657d86afeb10215719dff07
maheshvakkund3107 <maheshvakkund3107@outlook.com>
2021-12-01 00:20:30 +0530

    -Exercise on Object,Class to develop our own collection using Scala - Initial Commit

---
+++ src/main/scala/scalaoops/practiceexercise/MyList.scala
@@ -0,0 +1,50 @@
+package scalaoops.practiceexercise
+
+abstract class MyList {
+
+  /*
+  1.head : First element of the list.
+  2.tail : Remainder of the list.
+  3.isEmpty : Is this list empty.
+  4.add(int) : New list with this element added.
+  5.toString : a String representation of the list.
+   */
+
+  def head: Int
+  def tail: MyList
+  def isEmpty: Boolean
+  def add(element: Int): MyList
+  def printElements: String
+  override def toString: String = "[" + printElements + "]"
+}
+
+object Empty extends MyList {
+  def head: Int = throw new NoSuchElementException
+  def tail: MyList = throw new NoSuchElementException
+  def isEmpty: Boolean = true
+  def add(element: Int): MyList = {
+    new Cons(element, Empty)
+  }
+
+  override def printElements: String = ""
+}
+class Cons(h: Int, t: MyList) extends MyList {
+  def head: Int = h
+  def tail: MyList = t
+  def isEmpty: Boolean = false
+  def add(element: Int): MyList = {
+    new Cons(element, this)
+  }
+  override def printElements: String = {
+    if (t.isEmpty) "" + h
+    else h + " " + t.printElements
+
+  }
+}
+
+object ListTest extends App {
+  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  println(list.tail.head)
+  println(list.add(4).head)
+  println(list.toString)
+}
