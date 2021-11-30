2a89fc4f268399d4b1f74487143ee2cc1c1bac57
maheshvakkund3107 <maheshvakkund3107@outlook.com>
2021-11-30 18:48:07 +0530

    -Abstract datatype in Scala, Traits in Scala

--- src/main/scala/scalaoops/AbstractDataTypes.scala
+++ src/main/scala/scalaoops/AbstractDataTypes.scala
@@ -7,6 +7,7 @@ object AbstractDataTypes extends App {
    */
   abstract class Animal {
     val creatureType: String
+
     def eat: Unit
   }
 
@@ -15,8 +16,36 @@ object AbstractDataTypes extends App {
     Override keyword is non mandtory since the compiler will figure it out.
      */
     override val creatureType: String = "Canine"
+
     override def eat: Unit = {
       println("Dog is eating")
     }
   }
+
+  //Trait
+  trait Carnivore {
+    def eat(animal: Animal): Unit
+    val preferredMeat: String = "Fresh meat"
+  }
+
+  trait ColdBlooded {}
+  class Crocodile extends Animal with Carnivore {
+    val creatureType: String = "croc"
+
+    def eat: Unit = "Crocodile is eating"
+
+    def eat(animal: Animal): Unit = {
+      println(s"I am croc and I am eating ${animal.creatureType}")
+    }
+  }
+  val dog = new Dog
+  val croc = new Crocodile
+  croc.eat(dog)
+  /*
+  Difference between Abstract and Trait
+  1.Both Abstract classes and traits have both Abstract and Non Abstract members.
+  2.Traits cannot have constructor parameters.
+  3.We can inherit only one class at a time but inherit multiple traits at a given time.(Multiple inheritance can be achieved by traits).
+  4.Traits = Behavior, Abstract class=Thing.
+   */
 }
