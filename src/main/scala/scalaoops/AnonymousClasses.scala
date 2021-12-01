44f881d9ba9d0ad634daace72641be19d6cc7a38
maheshvakkund3107 <maheshvakkund3107@outlook.com>
2021-12-01 23:48:32 +0530

    -Anonymous Class in Scala.

--- src/main/scala/scalaoops/AnonymousClasses.scala
+++ src/main/scala/scalaoops/AnonymousClasses.scala
@@ -21,5 +21,22 @@ object AnonymousClasses {
 
     val funnyAnimal: Animal = new Animal
      */
+
+    class Person(name: String) {
+      def sayHi: Unit = {
+        println(s"Hi, my name is $name how may I help you?")
+      }
+    }
+
+    val jim = new Person("Jim") {
+      override def sayHi: Unit =
+        println("Hi ,my name is Jim , how can i be of service")
+    }
+
+    /*
+    Rules for Anonymous Class:
+    1. Pass in required constructor arguments if needed.
+    2. Implement all abstract fields/Methods.
+     */
   }
 }
