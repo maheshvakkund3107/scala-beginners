1707a3d3c68f1e8a6e482de968b95d83660f1839
maheshvakkund3107 <maheshvakkund3107@outlook.com>
2021-12-02 23:47:40 +0530

    -Enum in Scala 3.

---
+++ src/main/scala/scalaoops/Enums.scala
@@ -0,0 +1,49 @@
+package scalaoops
+
+object Enums{
+  /**
+   * enum is a datatype so we can add methods or fields.
+   */
+  enum Permissions {
+    case READ, WRITE, EXECUTE, NONE
+
+    def openDocument(): Unit = {
+      if (this == READ) println("Opening document")
+      else println("Reading not allowed")
+    }
+  }
+    val somePermission:Permissions=Permissions.READ
+
+
+  /**
+   * 1.Enumbs can take constructor args.
+   */
+  enum PermissionWithBits(bits:Int){
+    case READ extends PermissionWithBits(4)
+    case WRITE extends PermissionWithBits(2)
+    case EXECUTE extends PermissionWithBits(1)
+    case NONE extends PermissionWithBits(0)
+  }
+
+  /**
+   * 2.Enum can have companion objects.
+   */
+  object PermissionWithBits{
+    def fromBits(bits:Int):PermissionWithBits={
+      PermissionWithBits.NONE
+    }
+  }
+
+  /**
+   * Standard API for enum.
+   */
+  val somePermissionsOrdinal=somePermission.ordinal
+  val allPermissions=PermissionWithBits.values //array of all possible values of the enum
+  val readPermission = Permissions.valueOf("READ") //No arg constructor is required
+
+  def main(args: Array[String]): Unit = {
+    somePermission.openDocument()
+    println(somePermissionsOrdinal)
+    println(allPermissions)
+  }
+}
