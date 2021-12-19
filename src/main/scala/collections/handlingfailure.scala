c5ff8f95e3a546ea851c12db7ac14678c879560d
maheshvakkund <maheshvakkund3107@outlook.com>
2021-12-19 12:07:55 +0530

    -handling failure in scala

--- src/main/scala/collections/handlingfailure.scala
+++ src/main/scala/collections/handlingfailure.scala
@@ -1,6 +1,6 @@
 package collections
 
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Random, Success, Try}
 
 object handlingfailure extends App {
 
@@ -61,4 +61,63 @@ object handlingfailure extends App {
   println(sSuccess.flatMap(x => Success(x * 10)))
   println(sSuccess.filter(_ > 10))
 
+  /** Exercise
+    */
+
+  val hostname = "localhost"
+  val port = "8080"
+  def renderHTML(page: String): Unit = println(page)
+
+  class Connection {
+    def get(url: String): String = {
+      val random = new Random(System.nanoTime())
+      if (random.nextBoolean()) "<html></html>"
+      else throw new RuntimeException("Connection Interrupted")
+    }
+
+    def getSafe(url: String): Try[String] = Try(get(url))
+  }
+
+  object HttpService {
+    val random = new Random(System.nanoTime())
+    def getConnection(host: String, port: String): Connection = {
+      if (random.nextBoolean()) new Connection
+      else throw new RuntimeException("SomeOne else took the port")
+    }
+
+    def getSafeConnection(host: String, port: String): Try[Connection] = Try(
+      getConnection(host, port)
+    )
+  }
+
+  val possibleConnection = HttpService.getSafeConnection(hostname, port)
+  val possibleHtml =
+    possibleConnection.flatMap(connection => connection.getSafe("/home"))
+  possibleHtml.foreach(renderHTML)
+
+  /** Shorthand Version
+    */
+
+  HttpService
+    .getSafeConnection(hostname, port)
+    .flatMap(connection => connection.getSafe("/home"))
+    .foreach(renderHTML)
+
+  for {
+    connection <- HttpService.getSafeConnection(hostname, port)
+    html <- connection.getSafe("/home")
+  } renderHTML(html)
+
+  /** try{
+    *   connection = HttpService.getSafeConnection(hostname, port)
+    *    try{
+    *      page = connection.getSafe("/home")
+    *      renderHTML(html)
+    *    }catch (Exception e){
+    *
+    *    }
+    *  }catch (Exception e){
+    *
+    *  }
+    */
 }
