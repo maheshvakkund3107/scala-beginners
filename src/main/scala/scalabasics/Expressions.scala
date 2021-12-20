340d3f2cfd6b0925d759445fb2c7cf88d9327e1c
maheshvakkund <maheshvakkund3107@outlook.com>
2021-12-20 11:34:22 +0530

    -Improvised Expressions concept in  scala.

--- src/main/scala/scalabasics/Expressions.scala
+++ src/main/scala/scalabasics/Expressions.scala
@@ -1,5 +1,10 @@
 package scalabasics
 
+/** A unit of code that returns a value after it has been executed.
+  * One or more lines of code can be considered an expression
+  * if they are collected together using curly braces ({ and }).
+  * This is known as an expression block.
+  */
 object Expressions extends App {
   val x = 1 + 2
   println(1 + 2)
@@ -11,36 +16,48 @@ object Expressions extends App {
   // ! && ||
 
   var aVariable = 2
-  aVariable += 3 //Also works with -= *= /= Works only with var (Side Effects)
-  //Instructions
-  /*
-  Instructions are doing something.
-   */
-  //Expressions
-  /*
-    Expression is something which has value or types.
-    Expressions are evaluated
-    IF in scala is a recurssion
-   */
 
-  //If Expression
+  /** Also works with -= *= /= Works only with var (Side Effects)
+    */
+  aVariable += 3
+
+  /**  Instructions are doing something.
+    */
+
+  /**    Expression is something which has value or types.
+    *    Expressions are evaluated
+    *    IF in scala is a recurssion
+    */
+
+  /** If Expression
+    */
+
   val aCondition = true
   val aConditionValue = if (aCondition) 5 else 4
   println(aConditionValue)
 
-  //Never write loop
+  /** Never write loop
+    */
+
   var i = 0
   while (i < 10) {
     println(i)
     i += 1
   }
-  //Everything in Scala is a expression
-  val aWeirdValue = (aVariable = 3) // Unit === void
+
+  /** Everything in Scala is a expression
+    */
+
+  /** Unit === void
+    */
+  val aWeirdValue = (aVariable = 3)
   println(aWeirdValue)
 
-  //Side effects: println, whiles, reassigning
+  /** Side effects: println, whiles, reassigning
+    */
 
-  //Code Blocks
+  /**  Code Blocks
+    */
 
   val aCodeBlock = {
     val x = 2
