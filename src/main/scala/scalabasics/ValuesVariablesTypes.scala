8acdda93ca34a94eacb0d4e8b4aec7e67cefd401
maheshvakkund <maheshvakkund3107@outlook.com>
2021-12-20 11:24:19 +0530

    -Improvised Variables and Values concept in  scala.

--- src/main/scala/scalabasics/ValuesVariablesTypes.scala
+++ src/main/scala/scalabasics/ValuesVariablesTypes.scala
@@ -1,35 +1,41 @@
 package scalabasics
 
 /**
-  *
   */
 object ValuesVariablesTypes extends App {
   val x: Int = 10
   println(x)
 
-  /**
-    * x=12
+  /** x=12
     * vals are immutable, cannot be reassigned.
     */
 
   val y = 42
   println(y)
 
-  /**
-    * Compiler can infer types.
+  /** Compiler can infer types based on the data.
     */
 
-  val str: String = "hello";
+  val str: String = "hello"
 
-  /**
-    * Semicolons are allowed in scala
+  /** Semicolons are allowed in scala
+    *
+    * val str: String = "hello";
     */
 
   val aBoolean: Boolean = false
   val aChar: Char = 'a'
 
-  /**
-    * Compiler complains if number too long
+  /** Compiler complains if number too long.
+    *
+    * There are size for each data type in scala
+    *
+    * Boolean-1 Bit True or false.
+    * Int - 4bytes or 32bits
+    * Short- 2bytes or 16bits
+    * Long-8bytes or 64bits
+    * Float -4bytes or 32bits - Single Precision
+    * Double - 4bytes or 32 bits - Double precision
     */
 
   val aShort: Short = 1242
@@ -37,18 +43,19 @@ object ValuesVariablesTypes extends App {
   val aFloat: Float = 2.0f
   val aDouble: Double = 3.14
 
-  /**
-    *Variables
+  /** Variables
     */
 
   var aVariable: Int = 4
-  aVariable = 6 //Side Effects
 
-  /**
-    *Key Takeaways
-    *1: Prefer vals over vars
-    *2: All vals and vars have types
-    *3: Compiler automatically infers types when omitted
+  /** Side Effects.
+    */
+  aVariable = 6
+
+  /** Key Takeaways
+    * 1: Prefer vals over vars
+    * 2: All vals and vars have types
+    * 3: Compiler automatically infers types when omitted
     */
 
 }
