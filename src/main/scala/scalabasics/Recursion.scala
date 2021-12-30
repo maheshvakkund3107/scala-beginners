03db97ecbb04ffe841aaa2e27fbebe158b42db68
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2021-12-30 17:02:54 +0530

    --Initial commit for Improvised Recursion.scala

--- src/main/scala/scalabasics/Recursion.scala
+++ src/main/scala/scalabasics/Recursion.scala
@@ -3,88 +3,82 @@ package scalabasics
 import scala.annotation.tailrec
 
 /**
-  *
   */
 object Recursion extends App {
 
-  /**
-    *
-    * @param n
-    * @return
+  /** @param n-Number for which the factorial needs to be computed.
+    * @return - Factorial of a number.
     */
   def factorialNumber(n: Int): Int = {
     if (n <= 0) 1
     else n * factorialNumber(n - 1)
   }
 
-  /**
-    *
-    * @param n
-    * @return
+  /** @param n- Number for which the factorial needs to be computed.
+    * @return - Factorial of a number.
     */
   def anotherFactorial(n: Int): BigInt = {
+
+    /** @param x - In each call the number is decremented by 1.
+      * @param accumulator - Result/ Factorial after each call is stored in accumulator.
+      * @return - Factorial of a number.
+      */
     @tailrec
-    def factHelper(x: Int, accumalator: BigInt): BigInt = {
-      if (x <= 1) accumalator
-      else factHelper(x - 1, x * accumalator)
+    def factHelper(x: Int, accumulator: BigInt): BigInt = {
+      if (x <= 1) accumulator
+      else factHelper(x - 1, x * accumulator)
     }
     factHelper(n, 1)
 
   }
 
-  /**
-  anotherFactorial(10)=factHelper(10,1)
-  =factHelper(9,10*1)
-  =factHelper(8,10*9*1)
-  =factHelper(7,10*9*8*1)
-  ...
-  =factHelper(1,10*9*8*7...*1)
-  =1*2*3*4...*10
-    */
   println(anotherFactorial(5000))
 
-  /**
-    *
-    * @param str
-    * @param n
-    * @return
+  /** @param str -Input String which needs to be concatenated multiple times.
+    * @param n - Number of times the string needs to be concatenated.
+    * @return - A String concatenated multiple times.
     */
-  def stringNtimes(str: String, n: Int) = {
+  def stringTimes(str: String, n: Int) = {
+
+    /** @param x  - In each call the number is decremented by 1.
+      * @param accumulator- Result/ Factorial after each call is stored in accumulator.
+      * @param str - Input String which needs to be concatenated multiple times.
+      * @return - A String concatenated multiple times.
+      */
     @tailrec
-    def factHelper(x: Int, accumalator: String, str: String): String = {
-      if (x <= 1) accumalator
-      else factHelper(x - 1, accumalator.concat(str), str)
+    def factHelper(x: Int, accumulator: String, str: String): String = {
+      if (x <= 1) accumulator
+      else factHelper(x - 1, accumulator.concat(str), str)
     }
     factHelper(n, str, str)
   }
-  println(stringNtimes("mah", 5))
+  println(stringTimes("mah", 5))
 
-  /**
-    *
-    * @param n
+  /** @param n
     * @return
     */
   def primeNumber(n: Int): Boolean = {
+
+    /** @param t
+      * @param isStillPrime
+      * @return
+      */
     @tailrec
-    def isPrimeUntill(t: Int, isStillPrime: Boolean): Boolean = {
+    def isPrimeUntil(t: Int, isStillPrime: Boolean): Boolean = {
       if (!isStillPrime) false
       else if (t <= 1) true
-      else isPrimeUntill(t - 1, n % t != 0 && isStillPrime)
+      else isPrimeUntil(t - 1, n % t != 0 && isStillPrime)
     }
-    isPrimeUntill(n / 2, true)
+    isPrimeUntil(n / 2, true)
   }
   println(primeNumber(10))
 
-  /**
-    *
-    * @param n
+  /** @param n
     * @return
     */
   def fibonacciSeries(n: Int): Int = {
 
-    /**
-      *
-      * @param x
+    /** @param x
       * @param last
       * @param nextToLast
       * @return
