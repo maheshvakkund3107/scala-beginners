1471296416725a563509e8f1c4da785ea90af52f
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2021-12-30 16:36:23 +0530

    --Improvised Functions.scala

--- src/main/scala/scalabasics/Functions.scala
+++ src/main/scala/scalabasics/Functions.scala
@@ -1,35 +1,32 @@
 package scalabasics
 
+import scala.annotation.tailrec
+
 /**
-  *
   */
 object Functions extends App {
 
-  /**
-    *
-    * @param a
-    * @param b
-    * @return
+  /** @param a - A value of the String type.
+    * @param b- A value of the Int type.
+    * @return - A Concatenated String value
     */
   def aFunction(a: String, b: Int): String = {
     a + "" + b
   }
   println(aFunction("HELLO", 3))
 
-  /**
-    *
-    * @return
+  /** @return
+    * Functions can be called without the parameters.
+    * The round braces are too optional.
     */
   def aParameterlessFunction(): Int = 42
   println(aParameterlessFunction())
 
-  /**Recursive Functions*/
+  /** Recursive Functions */
 
-  /**
-    *
-    * @param aString
-    * @param n
-    * @return
+  /** @param aString -A value of the String type to be printed recursively.
+    * @param n-Number of times the string value to be repeated.
+    * @return - Returns a repeated string with specified number of times.
     */
   def repeatedFunction(aString: String, n: Int): String = {
     if (n == 1) aString
@@ -37,16 +34,19 @@ object Functions extends App {
   }
   println(repeatedFunction("hello", 3))
 
-  /**
-    * Key Takeaways
+  /** Key Takeaways
     * 1.When you need loops, use recursion
     * 2.In a normal function the return type is non mandatory,but in case of recursive function its mandatory
     * That is because the compiler cannot figure out the return type of a recursive function on its own.
-    *
     */
 
+  /** @param aString - Input Parameter of the type String.
+    */
   def aFunctionWithSideEffects(aString: String): Unit = print(aString)
 
+  /** @param n-Number of times the function needs to be called.
+    * @return
+    */
   def aBigFunction(n: Int): Int = {
     def aSmallFunction(a: Int, b: Int): Int = a + b
     aSmallFunction(n, n - 1)
@@ -54,13 +54,12 @@ object Functions extends App {
 
   println(aBigFunction(6))
 
-  /**Exercises*/
+  /** Exercises */
 
-  /**
-    *  1.A greeting function (name,age) =>Hi, my nameis $name and I am $age old
-    * @param name
-    * @param age
-    * @return
+  /**  1.A greeting function (name,age) =>Hi, my nameis $name and I am $age old
+    * @param name- - It takes input as Name.
+    * @param age - It takes input as Age.
+    * @return - Returns a String message.
     */
   def greetingFunction(name: String, age: Int): String = {
     "Hi, my name is " + name + "and I am " + age + "old"
@@ -68,8 +67,11 @@ object Functions extends App {
 
   println(greetingFunction("mahesh", 24))
 
-  /**
-    * 2. Prime Numbers
+  /** 2. Prime Numbers
+    */
+
+  /** @param n - It takes input as a number to calculate the factorial of the number (Int Type).
+    * @return - Factorial of the number.
     */
   def factorialNumber(n: Int): Int = {
     if (n <= 0) 1
@@ -77,8 +79,11 @@ object Functions extends App {
   }
   println(factorialNumber(4))
 
-  /**
-    * 3.Fibonacci Series
+  /** 3.Fibonacci Series
+    */
+
+  /** @param n- It takes input as a number to calculate the Fibonacci value of the number (Int Type).
+    * @return -Fibonacci Value
     */
   def fibonacciSeries(n: Int): Int = {
     if (n <= 2) 1
@@ -86,16 +91,20 @@ object Functions extends App {
   }
   println(fibonacciSeries(8))
 
-  /**
-    * 4.Prime Numbers
+  /** 4.Prime Numbers
+    */
+
+  /** @param n- It takes input as a number to check whether it is a prime number or not.
+    * @return - Boolean value of the result.
     */
   def primeNumber(n: Int): Boolean = {
-    def isPrimeUntill(t: Int): Boolean = {
+    @tailrec
+    def isPrimeUntil(t: Int): Boolean = {
       if (t <= 1) {
         true
-      } else { n % t != 0 && isPrimeUntill(t - 1) }
+      } else { n % t != 0 && isPrimeUntil(t - 1) }
     }
-    isPrimeUntill(n / 2)
+    isPrimeUntil(n / 2)
 
   }
   println(primeNumber(4))
