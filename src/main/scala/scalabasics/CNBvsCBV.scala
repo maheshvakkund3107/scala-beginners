ac86e1d19884a52583af5a78fff675a206940147
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2021-12-31 12:40:05 +0530

    --Improvised CNBvsCBV.scala

--- src/main/scala/scalabasics/CNBvsCBV.scala
+++ src/main/scala/scalabasics/CNBvsCBV.scala
@@ -1,27 +1,23 @@
 package scalabasics
 
 /**
-  *
   */
 object CNBvsCBV extends App {
 
-  /**
-    *
-    * 1.Value is computed before the call.
+  /** 1.Value is computed before the call.
     * 2.Same value is used everywhere.
     *
-    * @param x
+    * @param x- Takes a input value of long type.
     */
   def calledByValue(x: Long): Unit = {
     println("by Value :" + x)
     println("by Value:" + x)
   }
 
-  /**
-    * 1.Expression is passed iterally.
+  /** 1.Expression is passed internally.
     * 2.Expression is evaluated at every use within.
     *
-    * @param x
+    * @param x - Takes a input value of long type.
     */
   def calledByName(x: => Long): Unit = {
     println("by Name :" + x)
@@ -31,17 +27,13 @@ object CNBvsCBV extends App {
   calledByValue(System.nanoTime())
   calledByName(System.nanoTime())
 
-  /**
-    *
-    * @return
+  /** @return
     */
   def infinite(): Int = 1 + infinite()
 
-  /**
-    *
-    * @param x
-    * @param y
+  /** @param x- Takes a input value of Int type.
+    * @param y- Takes a input value of Int type.
     */
-  def printFirst(x: Int, y: => Int) = println(x)
+  def printFirst(x: Int, y: => Int): Unit = println(x)
   printFirst(34, infinite())
 }
