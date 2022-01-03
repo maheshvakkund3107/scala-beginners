1b7ac92eab57cb3438ad47c99a2da4c74111e036
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2022-01-03 23:22:22 +0530

    --Improvised AbstractDataTypes.scala

--- src/main/scala/scalaoops/AbstractDataTypes.scala
+++ src/main/scala/scalaoops/AbstractDataTypes.scala
@@ -1,34 +1,30 @@
 package scalaoops
 
 /**
-  *
   */
 object AbstractDataTypes extends App {
 
-  /**
-    * Abstract
+  /** Abstract
     * 1.Abstract class cannot be instantiated hence object creation impossible.
     */
 
   abstract class Animal {
     val creatureType: String
-    def eat: Unit
+    def eat(): Unit
   }
 
   class Dog extends Animal {
 
-    /**
-      *Override keyword is non mandtory since the compiler will figure it out.
+    /** Override keyword is non mandatory since the compiler will figure it out.
       */
     override val creatureType: String = "Canine"
 
-    override def eat: Unit = {
+    override def eat(): Unit = {
       println("Dog is eating")
     }
   }
 
-  /**
-    * Trait
+  /** Trait
     */
 
   trait Carnivore {
@@ -39,18 +35,20 @@ object AbstractDataTypes extends App {
   trait ColdBlooded {}
 
   class Crocodile extends Animal with Carnivore {
-    val creatureType: String = "croc"
-    def eat: Unit = "Crocodile is eating"
+    val creatureType: String = "crocodile"
+    def eat(): Unit = {
+      println("Crocodile is eating")
+    }
     def eat(animal: Animal): Unit = {
-      println(s"I am croc and I am eating ${animal.creatureType}")
+      println(s"I am crocodile and I am eating ${animal.creatureType}")
     }
   }
   val dog = new Dog
-  val croc = new Crocodile
-  croc.eat(dog)
+  val crocodile = new Crocodile
+  crocodile.eat()
+  crocodile.eat(dog)
 
-  /**
-    * Difference between Abstract and Trait
+  /** Difference between Abstract and Trait
     * 1.Both Abstract classes and traits have both Abstract and Non Abstract members.
     * 2.Traits cannot have constructor parameters.
     * 3.We can inherit only one class at a time but inherit multiple traits at a given time.(Multiple inheritance can be achieved by traits).
