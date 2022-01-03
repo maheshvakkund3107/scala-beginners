f5cfecda7c8b83fe292c24206e4e1929c660e357
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2022-01-03 23:14:44 +0530

    --Improvised Inheritance.scala

--- src/main/scala/scalaoops/Inheritance.scala
+++ src/main/scala/scalaoops/Inheritance.scala
@@ -2,8 +2,7 @@ package scalaoops
 
 object Inheritance extends App {
 
-  /**
-    * Single class inheritance.
+  /** Single class inheritance.
     * Access Modifiers available in scala are
     * 1.Public
     * 2.Protected
@@ -12,66 +11,64 @@ object Inheritance extends App {
     */
   sealed class Animal {
     val creatureType = "wild"
-    def eat = { println("Animal is Eating") }
+    def eat(): Unit = { println("Animal is Eating") }
   }
   class Cat extends Animal {
-    def crunch = {
-      eat
+    def crunch(): Unit = {
+      eat()
       println("Cat is crunching")
     }
   }
   val cat = new Cat()
-  cat.crunch
+  cat.crunch()
 
-  /**Constructors*/
-  /**Case 1:*/
+  /** Constructors */
+  /** Case 1: */
   class Person(name: String, age: Int)
 
-  /**
-    * 1.Before calling the constructor of Adult it will call the constructor of Person
+  /** 1.Before calling the constructor of Adult it will call the constructor of Person
     * 2.Constructor without the parameters is not available so it will through error.
     * class Adult(name: String, age: Int, idCard: String) extends Person
     */
   class Adult(name: String, age: Int, idCard: String)
       extends Person(name: String, age: Int)
 
-  /**Auxilary constructor*/
-  /**Case 2:
-  class PersonNew(name: String, age: Int) {
-    def this(name: String) = this(name, 0)
-  }
-  class AdultNew(name: String, age: Int, idCard: String)
-      extends PersonNew(name: String)
+  /** Auxiliary constructor */
+  /** Case 2:
+    *  class PersonNew(name: String, age: Int) {
+    *    def this(name: String) = this(name, 0)
+    *  }
+    *  class AdultNew(name: String, age: Int, idCard: String)
+    *      extends PersonNew(name: String)
     */
 
-  /**Overriding*/
-  /**Fields from the superclass can be overriden directly in the constructor of the child class */
+  /** Overriding */
+  /** Fields from the superclass can be overridden directly in the constructor of the child class */
   class Dog(override val creatureType: String) extends Animal {
 
-    /**override val creatureType: String = "domestic" */
-    override def eat = println("Dog is eating")
+    /** override val creatureType: String = "domestic" */
+    override def eat(): Unit = println("Dog is eating")
   }
   val dog = new Dog("domestic")
-  dog.eat
+  dog.eat()
   println(dog.creatureType)
 
-  /**Type Substitution : For the instance of Dog we can use the type as Animal this is called as Polymorphism*/
+  /** Type Substitution : For the instance of Dog we can use the type as Animal this is called as Polymorphism */
   val unknownAnimal: Animal = new Dog("K9")
-  unknownAnimal.eat
+  unknownAnimal.eat()
 
-  /**Super is used when you want to reference a method or a field from the parent class.*/
+  /** Super is used when you want to reference a method or a field from the parent class. */
   class Tiger extends Animal {
-    override def eat: Unit = {
-      super.eat
+    override def eat(): Unit = {
+      super.eat()
       println("Tiger is eating")
     }
   }
   val tiger = new Tiger
-  tiger.eat
+  tiger.eat()
 
-  /**Preventing Overrides.*/
-  /**
-    * 1.Use final keyword on the fields or members or methods.
+  /** Preventing Overrides. */
+  /** 1.Use final keyword on the fields or members or methods.
     * 2.Use final on the entire class - Stop the Extension to all classes.
     * 3.Seal the class - Extend classes in this file and prevent extension in other files.
     */
