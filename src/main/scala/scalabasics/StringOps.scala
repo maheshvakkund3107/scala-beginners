766e3e6696f43a176acb54bfc2f090f454d4887f
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2022-01-03 11:26:32 +0530

    --Improvised StringOps.scala

--- src/main/scala/scalabasics/StringOps.scala
+++ src/main/scala/scalabasics/StringOps.scala
@@ -12,25 +12,26 @@ object StringOps extends App {
 
   val aNumberString = "45"
   val aNumber = aNumberString.toInt
-  /*Scala Specific: String interpolators*/
+
+  /** Scala Specific: String interpolators */
   println('a' +: aNumberString :+ 'z')
   println(str.reverse)
   println(str.take(2))
 
-  /*S-interpolators*/
+  /** S-interpolator */
   val name = "David"
   val age = 12
   val greeting = s"Hello, my name is $name and  I am age $age years old"
-  val anothergreeting =
+  val anotherGreeting =
     s"Hello, my name is $name and  I am age ${age + 1} years old"
-  println(anothergreeting)
+  println(anotherGreeting)
 
-  /*F-interpolators*/
+  /** F-interpolator */
   val speed = 1.2f
   val myth = f"$name can eat $speed%2.2f burgers per minute"
   println(myth)
 
-  /*raw-interpolator*/
+  /** raw-interpolator */
   println(raw"This is a \n new line")
   val escaped = "This is a \n new line"
   println(raw"$escaped")
