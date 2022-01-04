71ee36980f8fce6f2b41ce10532501fc9cc5afa6
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2022-01-04 12:26:57 +0530

    --Added Generics to  MyList.scala

--- src/main/scala/scalaoops/MyList.scala
+++ src/main/scala/scalaoops/MyList.scala
@@ -1,14 +1,14 @@
 package scalaoops
 
-abstract class MyList {
+abstract class MyList[+A] {
 
   /** head -> first element of the list.
     */
-  def head: Int
+  def head: A
 
   /** tail -> remainder of the list.
     */
-  def tail: MyList
+  def tail: MyList[A]
 
   /** isEmpty -> Is the list empty.
     */
@@ -16,7 +16,7 @@ abstract class MyList {
 
   /** add(int) -> new list with this element added.
     */
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
 
   /** toString -> a string representation of the list.
     */
@@ -29,15 +29,15 @@ abstract class MyList {
 
 }
 
-object Empty extends MyList {
+object Empty extends MyList[Nothing] {
 
   /** @return - For a empty list there is no head so we throw the NoSuchElementException.
     */
-  override def head: Int = throw new NoSuchElementException
+  override def head: Nothing = throw new NoSuchElementException
 
   /** @return - For a empty list there is no tail so we throw the NoSuchElementException.
     */
-  override def tail: MyList = throw new NoSuchElementException
+  override def tail: MyList[Nothing] = throw new NoSuchElementException
 
   /** @return - Since the list is empty we return true.
     */
@@ -46,7 +46,8 @@ object Empty extends MyList {
   /** @param element - Element contains the value to be added to MyList.
     * @return
     */
-  override def add(element: Int): MyList = new Cons(element, Empty)
+  def add[B >: Nothing](element: B): MyList[B] =
+    new Cons(element, Empty)
 
   /** @return
     */
@@ -54,15 +55,15 @@ object Empty extends MyList {
 
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
 
   /** @return - Will return the head for MyList Object.
     */
-  override def head: Int = h
+  override def head: A = h
 
   /** @return - Will return the tail for MyList Object.
     */
-  override def tail: MyList = t
+  override def tail: MyList[A] = t
 
   /** @return - Since the list is not empty we return true.
     */
@@ -71,7 +72,7 @@ class Cons(h: Int, t: MyList) extends MyList {
   /** @param element- element contains the value to be added to MyList.
     * @return
     */
-  override def add(element: Int): MyList = new Cons(element, this)
+  override def add[B >: A](element: B): MyList[B] = new Cons(element, this)
 
   /** @return
     */
@@ -81,9 +82,10 @@ class Cons(h: Int, t: MyList) extends MyList {
 }
 
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(list.tail.head)
-  println(list.add(4).head)
-  println(list.isEmpty)
-  println(list.toString)
+  val listOfIntegers: MyList[Int] =
+    new Cons[Int](1, new Cons[Int](2, new Cons[Int](3, Empty)))
+  val listOfString: MyList[String] =
+    new Cons[String]("Hello", new Cons[String]("World", Empty))
+  println(listOfIntegers.toString)
+  println(listOfString.toString)
 }
