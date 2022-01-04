478d43f4b52cf2d15f372f6ad7e89bd7bf729ff5
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2022-01-04 12:49:44 +0530

    --Improvised AnonymousClasses.scala

--- src/main/scala/scalaoops/AnonymousClasses.scala
+++ src/main/scala/scalaoops/AnonymousClasses.scala
@@ -1,48 +1,46 @@
 package scalaoops
 
 /**
-  *
   */
 object AnonymousClasses {
 
-  /**
-    *
-    * @param args
+  /** @param args - Arguments for the main method.
     */
   def main(args: Array[String]): Unit = {
 
     abstract class Animal {
-      def eat: Unit
+      def eat(): Unit
     }
-    val funnyAnimal: Animal = new Animal {
-      override def eat: Unit = println("hahahaha")
-    }
+
+    /** val funnyAnimal: Animal = new Animal {
+      *      override def eat(): Unit = println("haha haha")
+      *    }
+      */
+    val funnyAnimal: Animal = () => println("haha haha")
 
     println(funnyAnimal.getClass)
 
-    /**
-      * Behind the scenes when a Anonymous class is created.
-      class AnonymousClasses$$anon$1 extends Animal{
-        override def eat: Unit = println("hahahaha")
-      }
-      val funnyAnimal: Animal = new Animal
+    /** Behind the scenes when a Anonymous class is created.
+      *      class AnonymousClasses$$anon$1 extends Animal{
+      *        override def eat: Unit = println("haha haha")
+      *      }
+      *      val funnyAnimal: Animal = new Animal
       */
 
     class Person(name: String) {
-      def sayHi: Unit = {
+      def sayHi(): Unit = {
         println(s"Hi, my name is $name how may I help you?")
       }
     }
 
-    val jim = new Person("Jim") {
-      override def sayHi: Unit =
+    val jim: Person = new Person("Jim") {
+      override def sayHi(): Unit =
         println("Hi ,my name is Jim , how can i be of service")
     }
 
     print(jim)
 
-    /**
-      * Rules for Anonymous Class:
+    /** Rules for Anonymous Class:
       * 1. Pass in required constructor arguments if needed.
       * 2. Implement all abstract fields/Methods.
       */
