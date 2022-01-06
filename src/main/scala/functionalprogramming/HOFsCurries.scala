0d8c1efde51f3431b39cf1fc6f57791b0020d588
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2022-01-06 10:34:18 +0530

    Update HOFsCurries.scala
    -Improvised HOFsCUrries.scala.

--- src/main/scala/functionalprogramming/HOFsCurries.scala
+++ src/main/scala/functionalprogramming/HOFsCurries.scala
@@ -4,16 +4,15 @@ import scala.annotation.tailrec
 
 object HOFsCurries extends App {
 
-  val superFunction: (Int, (String, (Int, Boolean)) => Int) => (Int => Int) =
+  val superFunction: (Int, (String, (Int, Boolean)) => Int) => Int => Int =
     null
 
-  /**
-    *
-    * @param f
-    * @param n
-    * @param x
+  /** @param f - Function.
+    * @param n - Number of times.
+    * @param x - Value.
     * @return
     */
+  @tailrec
   def nTimes(f: Int => Int, n: Int, x: Int): Int = {
     if (n <= 0) x
     else nTimes(f, n - 1, f(x))
@@ -21,27 +20,26 @@ object HOFsCurries extends App {
   val plusOne = (x: Int) => x + 1
   println(nTimes(plusOne, 10, 1))
 
-  /**
-    *
-    * @param f
-    * @param n
+  /** @param f - Function.
+    * @param n - Number of times.
     * @return
     */
 
-  def nTimesBetter(f: Int => Int, n: Int): (Int => Int) = {
+  def nTimesBetter(f: Int => Int, n: Int): Int => Int = {
     if (n <= 0) (x: Int) => x
     else
       (x: Int) => nTimesBetter(f, n - 1)(f(x))
   }
+  val plus10 = nTimesBetter(plusOne, 10)
+  println(plus10(1))
 
   def curriedFormatter(c: String)(x: Double): String = c.format(x)
-  val standardFormat: (Double => String) = curriedFormatter("%4.2f")
-  val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
+  val standardFormat: Double => String = curriedFormatter("%4.2f")
+  val preciseFormat: Double => String = curriedFormatter("%10.8f")
   println(standardFormat(Math.PI))
   println(preciseFormat(Math.PI))
 
-  /**
-    * Takeaways:
+  /** Takeaways:
     * 1.Functional programming=working with functions
     *     -Pass functions as parameters.
     *     -Return functions as result.
