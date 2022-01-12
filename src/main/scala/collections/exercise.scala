d30a8e0c460aeab5f8296d2eeaae54e2efda8d80
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2022-01-12 14:54:16 +0530

    Update exercise.scala
    Update exercise.scala

--- src/main/scala/collections/exercise.scala
+++ src/main/scala/collections/exercise.scala
@@ -1,47 +1,71 @@
 package collections
 
+import scala.annotation.tailrec
+
 /**
- *
- */
+  */
 object exercise extends App {
-  /**
-   *
-   * @param network
-   * @param person
-   * @return
-   */
-  def add(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+
+  /** Here we add a new person to the Map.
+    * network here is a map to this map we add a key as person which is string and the value of the key is a new Set.
+    * @param network - Receives a network object which is a Map[String,Set[String]
+    * @param person - Receives a Person which is a String
+    * @return
+    */
+  def add(
+      network: Map[String, Set[String]],
+      person: String
+  ): Map[String, Set[String]] = {
     network + (person -> Set())
   }
 
-  /**
-   *
-   * @param network
-   * @param a
-   * @param b
-   * @return
-   */
-  def friend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+  /** Here we get the friends of A, friends of B from the network map which we got.
+    * Then we add the friends of  B to A and friends of A to B
+    * @param network- Receives a network object which is a Map[String,Set[String]
+    * @param a- Receives a Person which is a String
+    * @param b- Receives a Person which is a String
+    * @return
+    */
+  def friend(
+      network: Map[String, Set[String]],
+      a: String,
+      b: String
+  ): Map[String, Set[String]] = {
     val friendsA = network(a)
     val friendsB = network(b)
     network + (a -> (friendsA + b)) + (b -> (friendsB + a))
   }
 
-  /**
-   *
-   * @param network
-   * @param a
-   * @param b
-   * @return
-   */
-  def unfriend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+  /** Here we get the friends of A, friends of B from the network map which we got.
+    * Then we remove the friends of  B to A and friends of A to B
+    * @param network- Receives a network object which is a Map[String,Set[String]
+    * @param a- Receives a Person which is a String
+    * @param b- Receives a Person which is a String
+    * @return
+    */
+  def unfriend(
+      network: Map[String, Set[String]],
+      a: String,
+      b: String
+  ): Map[String, Set[String]] = {
     val friendsA = network(a)
     val friendsB = network(b)
     network + (a -> (friendsA - b)) + (b -> (friendsB - a))
   }
 
-  def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
-    def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] = {
+  /** @param network- Receives a network object which is a Map[String,Set[String]
+    * @param person- Receives a Person which is a String
+    * @return
+    */
+  def remove(
+      network: Map[String, Set[String]],
+      person: String
+  ): Map[String, Set[String]] = {
+    @tailrec
+    def removeAux(
+        friends: Set[String],
+        networkAcc: Map[String, Set[String]]
+    ): Map[String, Set[String]] = {
       if (friends.isEmpty) networkAcc
       else removeAux(friends.tail, unfriend(networkAcc, person, friends.head))
     }
