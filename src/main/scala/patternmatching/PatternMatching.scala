a3895e7472a33d11c41fb5e8e19ed8acf47f3592
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2022-01-17 13:19:43 +0530

    Updated Pattern Matching scala files
    Updated Pattern Matching scala files

--- src/main/scala/patternmatching/PatternMatching.scala
+++ src/main/scala/patternmatching/PatternMatching.scala
@@ -20,8 +20,8 @@ object PatternMatching extends App {
 
   /** 1.Decompose values.
     *
-    * @param name
-    * @param age
+    * @param name - Takes a name as a parameter.
+    * @param age- Takes a age as a parameter.
     */
   case class Person(name: String, age: Int)
   val bob = Person("mahesh", 15)
@@ -29,25 +29,28 @@ object PatternMatching extends App {
   val greeting = bob match {
     case Person(n, a) if a < 21 => s"Hi my name is $n and I cant drink"
     case Person(n, a)           => s"Hi my name is $n and I am $a years old"
-    case _                      => "I dont know you"
+    case null                   => "I don't know you"
   }
   println(greeting)
 
   /** 1.CASES are matched in order.
-    * 2. If cases are not matched we get MATCHERROR.
-    * 3.Type of PM Expression? Unified type of all the types in all the cases.
+    * 2.If cases are not matched we get scala.MatchError.
+    * 3.Type of PatternMatching Expression? Unified type of all the types in all the cases.
     * 4.Pattern matching really works well with case classes.
     */
 
-  /** Pattern matching on sealed hierrachies.
+  /** Pattern matching on sealed hierarchies.
     */
 
+  /** Pattern matching for sealed classes needs exhaustive pattern, else compiler will highlight it.
+    */
   sealed class Animal
   case class Dog(breed: String) extends Animal
   case class Parrot(greeting: String) extends Animal
   val animal: Animal = Dog("Terra Nova")
   animal match {
     case Dog(someBreed) => println(s"Matched a dog of the $someBreed breed")
+    case _              => println("I don't know you")
   }
 
   val i = 6
@@ -65,16 +68,14 @@ object PatternMatching extends App {
   def show(e: Expr): String = e match {
     case Number(n)   => s"$n"
     case Sum(e1, e2) => show(e1) + "+" + show(e2)
-    case Prod(e1, e2) => {
-      def mayBeShowParanthesis(exp: Expr) =
+    case Prod(e1, e2) =>
+      def mayBeShowParentheses(exp: Expr) =
         exp match {
           case Prod(_, _) => show(exp)
           case Number(_)  => show(exp)
           case _          => "(" + show(exp) + ")"
         }
-
-      mayBeShowParanthesis(e1) + "*" + mayBeShowParanthesis(e2)
-    }
+      mayBeShowParentheses(e1) + "*" + mayBeShowParentheses(e2)
   }
   println(show(Sum(Number(2), Number(3))))
   println(show(Sum(Sum(Number(2), Number(3)), Number(4))))
